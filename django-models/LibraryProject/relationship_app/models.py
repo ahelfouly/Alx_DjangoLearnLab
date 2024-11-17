2d14fd39735c48b124f34a1bb5a10511da1542d2
Ahmed Elfouly <ahmedelfouly1999@gmail.com>
2024-11-17 16:25:30 +0200

    completing week 10

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -15,11 +15,11 @@ class Book(models.Model):
         return self.title
     
     class Meta:
-    permissions = [
-        ('can_add_book', 'Can add book'),           
-        ('can_change_book', 'Can change book'),      
-        ('can_delete_book', 'Can delete book'), 
-        ]
+        permissions = [
+            ('can_add_book', 'Can add book'),           
+            ('can_change_book', 'Can change book'),      
+            ('can_delete_book', 'Can delete book'), 
+            ]
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
